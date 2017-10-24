94be7c6cd7ffc0f5fba6096089c4f42d344ed971
jakubczachor7 <jakubczachor7@gmail.com>
2017-10-24 13:36:58 +0200

    added fonts to assetManager

--- kolkoKrzyzyk/AssetManager.cpp
+++ kolkoKrzyzyk/AssetManager.cpp
@@ -5,12 +5,25 @@ void AssetManager::LoadTexture(std::string name, std::string fileName)
 	sf::Texture tex;
 	if (tex.loadFromFile(fileName)) {
 		this->textures[name] = tex;
-	}
+	}else throw std::invalid_argument("missing texture file!");
+}
+
+void AssetManager::LoadFont(std::string name, std::string fileName)
+{
+	sf::Font font;
+	if (font.loadFromFile(fileName)) {
+		this->fonts[name] = font;
+	}else throw std::invalid_argument("missing font file!");
 }
 
 sf::Texture & AssetManager::GetTexture(std::string name)
 {
 	return this->textures.at(name);
+}
+
+sf::Font & AssetManager::GetFont(std::string name)
+{
+	return this->fonts.at(name);
 }
 
 void AssetManager::RemoveTexture(std::initializer_list<std::string> stringList)
@@ -19,6 +32,15 @@ void AssetManager::RemoveTexture(std::initializer_list<std::string> stringList)
 	for (std::string removed :stringList) {
 		it = this->textures.find(removed);
 		this->textures.erase(it);
+	}
+}
+
+void AssetManager::RemoveFont(std::initializer_list<std::string> fontList)
+{
+	std::map<std::string, sf::Font>::iterator it;
+	for (std::string removed : fontList) {
+		it = this->fonts.find(removed);
+		this->fonts.erase(it);
 	}
 }
 
