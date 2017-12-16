d47fa8953761923126850e06a84ec6cd9bf937c0
jakubczachor7 <jakubczachor7@gmail.com>
2017-12-16 21:16:42 +0100

    s

--- kolkoKrzyzyk/AI.hpp
+++ kolkoKrzyzyk/AI.hpp
@@ -10,6 +10,13 @@ public:
 	AI();
 	void analyze(std::vector<std::vector<char>> tab);
 	sf::Vector2i getHighestPoint();
+
+	void findBotMove();
+	void getValue_Position();
+	int CheckValue(int xd, int yd, int vx, int vy, int pl);
+	void make_a_move();
+
+
 private:
 
 };
